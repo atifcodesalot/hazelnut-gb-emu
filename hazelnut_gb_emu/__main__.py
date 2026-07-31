9b394d20481d7eb8d2a211fd738f536f932ef4cf
unhappygirl <johnlennonimaginednoheaven@gmail.com>
2026-07-31 20:24:10 +0300

    fix profiling

--- hazelnut_gb_emu/__main__.py
+++ hazelnut_gb_emu/__main__.py
@@ -6,12 +6,10 @@ from . import logger
 import sys
 import cProfile
 import threading
-import os
+import time
 
 
 IMPLEMENTED = [0x00, 0x01, 0x02, 0x3, 0x11, 0x12, 0x13]
-
-
 
 
 def init():
@@ -19,6 +17,7 @@ def init():
     reader = CartridgeReader(sys.argv[1])
     cartridge = reader.get_cartridge()
     return gb, cartridge
+
 
 def run_profiled(target, filename):
     profiler = cProfile.Profile()
@@ -34,27 +33,25 @@ def run_profiled(target, filename):
 def main():
     gb, cart = init()
     controller = SessionController(gameboy=gb, cartridge=cart)
-    controller.check_implementations(cartridge_types=CARTRIDGE_TYPES, implemented=IMPLEMENTED)
+    controller.check_implementations(
+        cartridge_types=CARTRIDGE_TYPES, implemented=IMPLEMENTED)
     controller.try_loading_save()
     try:
         if sys.argv[2] in ["--benchmark", "-b", "--profile", "-p"]:
             logger.info("Running in benchmark mode... Will run much slower than normal,\
                     but will generate a profile_stats file.")
-            
-            emu_target = lambda: run_profiled(
-            controller.emulate,
-            "profile_stats"
+
+            def emu_target(): return run_profiled(
+                controller.emulate,
+                "profile_stats"
             )
 
-            threading.Thread(target=emu_target).start()
             threading.Thread(target=controller.display).start()
+            time.sleep(2)
+            threading.Thread(target=emu_target).start()
 
-        else:
-            controller.play()
     except IndexError:
         controller.main()
-        
-        
 
 
 if __name__ == "__main__":
